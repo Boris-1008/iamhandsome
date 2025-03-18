40a85a19c2b5150ba2ff2c3f985e20b33b53562e
Boris-1008 <boris0905146617@gmail.com>
2025-03-19 00:09:43 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int N;
+    int i, j, num;
+    int lotto[8]; 
+    int count;
+    int copy;
+    srand(1);
+    scanf("%d", &N);
+
+    FILE *fp = fopen("lotto.txt", "w");
+    if (fp == NULL) {
+        return 1;
+    }
+
+    fprintf(fp, "========= lotto649 =========\n");
+
+    for (i = 1; i <= 5; i++) {
+        fprintf(fp, "[%d]: ", i);
+        if (i <= N) {
+            count = 0;
+            while (count < 7) {
+                num = rand() % 69 + 1;  
+                copy = 0;
+                for (j = 0; j < count; j++) {
+                    if (lotto[j] == num) {
+                        copy = 1;
+                        break;
+                    }
+                }
+                if (!copy) {
+                    lotto[count++] = num;
+                }
+            }
+            for (j = 0; j < 6; j++) {
+                fprintf(fp, "%02d ", lotto[j]);
+            }
+            fprintf(fp, "%02d", lotto[j]);
+        } 
+        else {
+            for (j = 0; j < 6; j++) {
+                fprintf(fp, "__ ");
+            }
+            fprintf(fp,"__");
+        }
+        fprintf(fp, "\n");
+    }
+    fprintf(fp, "========= csie@CGU =========");
+    fclose(fp);
+    return 0;
+}
